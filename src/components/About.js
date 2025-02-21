b65620b93c1006f3202a9b4c87bdbeed881bb413
Kyrylo Kozlovskyi <127797511+KyryloKozlovskyi@users.noreply.github.com>
2025-02-21 18:32:03 +0000

    Created the About component for the about page on the website.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Component for the About page
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website. Here is some information about us.</p>
+    </div>
+  );
+};
+
+export default About;
