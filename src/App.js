2c00ef309d956e8a5d2af846600543f8bd9a6e0a
Kyrylo Kozlovskyi <127797511+KyryloKozlovskyi@users.noreply.github.com>
2025-03-01 21:40:44 +0000

    Implemented AdminPanel component.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Submit from "./components/Submit";
-import SeeRecords from "./components/SeeRecords";
-import FilesMenu from "./components/EventMenu.js";
+import SeeRecords from "./components/AdminViews/SeeRecords";
+import EventMenu from "./components/AdminViews/EventMenu";
+import EventCreate from "./components/AdminViews/EventCreate.js";
+import EventUpdate from "./components/AdminViews/EventUpdate.js";
+import AdminPanel from "./components/AdminViews/AdminPanel";
 
 function App() {
   return (
@@ -16,7 +19,10 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/submit" element={<Submit />} />
         <Route path="/records" element={<SeeRecords />} />
-        <Route path="/events" element={<FilesMenu />} />
+        <Route path="/events" element={<EventMenu />} />
+        <Route path="/events/create" element={<EventCreate />} />
+        <Route path="/events/update/:id" element={<EventUpdate />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
       <Footer />
     </Router>
