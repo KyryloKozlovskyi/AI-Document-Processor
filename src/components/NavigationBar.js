7fe27516151d9935231571bce2c189cfdd0995c5
Kyrylo Kozlovskyi <127797511+KyryloKozlovskyi@users.noreply.github.com>
2025-03-01 21:37:30 +0000

    Updated NavigationBar component to include link to the admin page.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -9,9 +9,13 @@ const NavigationBar = () => {
         <Navbar.Brand href="/">Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/events">Events</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/submit">Submit</Nav.Link>
           <Nav.Link href="/records">Records</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/admin">Admin Panel</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
