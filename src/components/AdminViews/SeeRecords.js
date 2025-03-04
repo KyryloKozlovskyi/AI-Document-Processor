a0a54bcefa96bfb935fa30f298c399195dbb5bc7
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-04 17:25:44 +0000

    Fetch events from server and add event filtering to SeeRecords component

--- src/components/AdminViews/SeeRecords.js
+++ src/components/AdminViews/SeeRecords.js
@@ -7,6 +7,19 @@ const SeeRecords = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const [filterBy, setFilterBy] = useState("date");
+  const[events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/events");
+        setEvents(response.data);
+      } catch (err) {
+        console.error("Events fetch error:", err);
+      }
+    };
+    fetchEvents();
+  }, []);
 
   useEffect(() => {
     const fetchRecords = async () => {
@@ -69,6 +82,10 @@ const SeeRecords = () => {
         return filteredRecords.sort(
           (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
         );
+        case "event":
+          return filteredRecords.sort(
+            (a, b) => a.eventId.localeCompare(b.eventId)
+          );
       default:
         return filteredRecords;
     }
@@ -96,6 +113,7 @@ const SeeRecords = () => {
             <option value="date">Filter by Date</option>
             <option value="name">Filter by Name</option>
             <option value="type">Filter by Type</option>
+            <option value="type">Filter by Event</option>
           </Form.Select>
         </Form.Group>
       </div>
@@ -103,6 +121,7 @@ const SeeRecords = () => {
         <thead>
           <tr>
             <th>Type</th>
+            <th>Event</th>
             <th>Name</th>
             <th>Email</th>
             <th>File</th>
@@ -116,6 +135,10 @@ const SeeRecords = () => {
                 <Badge bg={record.type === "person" ? "primary" : "success"}>
                   {record.type}
                 </Badge>
+              </td>
+              <td>
+                {events.find((event) => event._id === record.eventId)
+                  ?.courseName}
               </td>
               <td>{record.name}</td>
               <td>{record.email}</td>
