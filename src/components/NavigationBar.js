d79a74981f872cb9790362965bfc58a27797f667
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-04-16 16:43:28 +0100

    Updated navbar styles to include icons, added toggler responsive design.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,33 +1,56 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import { useAdmin } from "./AdminViews/AdminContext";
+// Import icons from React Icons
+import {
+  FaHome,
+  FaFileUpload,
+  FaInfoCircle,
+  FaUserShield,
+  FaSignOutAlt,
+  FaSignInAlt,
+} from "react-icons/fa";
 
 const NavigationBar = () => {
   const { isAdmin, logout } = useAdmin();
 
   return (
-    <Navbar bg="primary" data-bs-theme="dark">
-      <Navbar.Brand className="p-2" href="/">
-        Navbar
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link className="px-2" href="/">Home</Nav.Link>
-        <Nav.Link className="px-2" href="/submit">Submit</Nav.Link>
-        <Nav.Link className="px-2" href="/about">About</Nav.Link>
-      </Nav>
-      <Nav>
-        {isAdmin ? (
-          <>
-            <Nav.Link href="/admin">Admin Panel</Nav.Link>
-            <Nav.Link className="px-4" onClick={logout}>Logout</Nav.Link>
-          </>
-        ) : (
-          <Nav.Link className="px-4" href="/login">
-            Login
-          </Nav.Link>
-        )}
-      </Nav>
+    <Navbar bg="primary" data-bs-theme="dark" expand="md" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">AI-Document-Processor</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">
+              <FaHome className="me-1" /> Home
+            </Nav.Link>
+            <Nav.Link href="/submit">
+              <FaFileUpload className="me-1" /> Submit
+            </Nav.Link>
+            <Nav.Link href="/about">
+              <FaInfoCircle className="me-1" /> About
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            {isAdmin ? (
+              <>
+                <Nav.Link href="/admin">
+                  <FaUserShield className="me-1" /> Admin Panel
+                </Nav.Link>
+                <Nav.Link onClick={logout}>
+                  <FaSignOutAlt className="me-1" /> Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link href="/login">
+                <FaSignInAlt className="me-1" /> Login
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
